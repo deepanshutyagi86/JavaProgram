f3edaf2041ea2034c574b91c595f956f5a2e1dea
deepanshutyagi86 <deepanshutyagi0784@gmail.com>
2024-06-28 14:32:55 +0530

    init test

--- src/App.java
+++ src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
